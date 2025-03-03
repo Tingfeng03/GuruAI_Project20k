05613be96525d13051cbc4def162c7aba19b377f
hngkhai <hngbob777@gmail.com>
2025-03-03 10:31:11 +0800

    roughly set up fastapi

--- TravelLah/python-backend/src/main.py
+++ TravelLah/python-backend/src/main.py
@@ -23,3 +23,28 @@ workflow.add_edge("generate", END)
 
 
 app = workflow.compile()
+
+
+from fastapi import FastAPI,APIRouter
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+app = FastAPI()
+router = APIRouter() 
+origins = [
+    "http://localhost:3000",
+    "localhost:3000"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+# Define root route
+@app.get("/")
+def read_root():
+    return {"message": "Hello, world!"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
