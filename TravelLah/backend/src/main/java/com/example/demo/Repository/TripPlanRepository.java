b51a922889a2531204062db85294ba0ece2cbe4d
Chong Kwang Chen <kwangchen.coding@gmail.com>
2025-03-12 19:35:30 +0800

    Resolve conflict

--- TravelLah/backend/src/main/java/com/example/demo/Repository/TripPlanRepository.java
+++
@@ -1,7 +0,0 @@
-// package com.example.demo.Repository;
-
-// import com.example.demo.model.TripPlan;
-// import org.springframework.data.mongodb.repository.MongoRepository;
-
-// public interface TripPlanRepository extends MongoRepository<TripPlan, String> {
-// }
